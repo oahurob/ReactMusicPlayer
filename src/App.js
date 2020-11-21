b1e8ce8b997653802001c7c23e53e623f6edf917
Pablo Gros <rob.r.pete@gmail.com>
2020-11-21 18:37:49 -0500

    added util.js, state song and currentsong to app.js, changed song.js to use util data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './styles/app.scss';
 //Adding Components
 import Player from './components/Player';
 import Song from './components/Song';
+import data from './util';
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[4]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong}/>
       <Player />
     </div>
   );
